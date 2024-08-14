7658190033558b569a779270805e69d945139340
Sai Pavan Kothamasu <SaiPavan.Kothamasu@blueyonder.com>
2024-08-14 16:03:29 +0530

    More changes

--- src/main/java/com/cron/parser/CronParserExpression.java
+++ src/main/java/com/cron/parser/CronParserExpression.java
@@ -2,22 +2,20 @@ package com.cron.parser;
 
 import static java.lang.String.format;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A CronExpression is a representation of a cron string with all members:
  * [minute] [hour] [day of month] [month] [day of week] [command]
- *
+ * <p>
  * It will validate basic syntax and delegate calculation of each member of the cron string (month, year ) to CronField,
  * which in turn will calculate the relevant exploded times (e.g. 1-3 will be 1,2,3 ) .
- *
  */
 public class CronParserExpression {
 
-    private CronParserField minutes;
-    private CronParserField hours;
-    private CronParserField dayOfMonth;
-    private CronParserField month;
-    private CronParserField dayOfWeek;
+    private List<CronParserField> fields;
 
     private String command;
 
@@ -27,25 +25,23 @@ public class CronParserExpression {
             throw new InvalidCronParserFieldException(
                 "Expected [minute] [hour] [day of month] [month] [day of week] [command] but got :" + arg);
         }
-
-        minutes = new CronParserField(cronMembers[0], CronParserFieldType.MINUTES);
-        hours = new CronParserField(cronMembers[1], CronParserFieldType.HOURS);
-        dayOfMonth = new CronParserField(cronMembers[2], CronParserFieldType.DAY_OF_MONTH);
-        month = new CronParserField(cronMembers[3], CronParserFieldType.MONTH);
-        dayOfWeek = new CronParserField(cronMembers[4], CronParserFieldType.DAY_OF_WEEK);
-        command = cronMembers[5];
+        fields = new ArrayList<>();
+        for (CronParserFieldType type : CronParserFieldType.values()) {
+            fields.add(new CronParserField(cronMembers[type.ordinal()], type));
+        }
+        command = cronMembers[cronMembers.length - 1];
 
     }
 
     public String toString() {
-        StringBuffer b = new StringBuffer();
-        b.append(format("%-14s%s\n", "minute", minutes.toString()));
-        b.append(format("%-14s%s\n", "hour", hours.toString()));
-        b.append(format("%-14s%s\n", "day of month", dayOfMonth.toString()));
-        b.append(format("%-14s%s\n", "month", month.toString()));
-        b.append(format("%-14s%s\n", "day of week", dayOfWeek.toString()));
-        b.append(format("%-14s%s\n", "command", command));
-        return b.toString();
+        StringBuilder output = new StringBuilder();
+        int i = 0;
+        for (CronParserFieldType type : CronParserFieldType.values()) {
+            output.append(format("%-14s%s\n", type.description, fields.get(i).toString()));
+            i++;
+        }
+        output.append(format("%-14s%s\n", "command", command));
+        return output.toString();
     }
 
 }
--- src/main/java/com/cron/parser/CronParserFieldType.java
+++ src/main/java/com/cron/parser/CronParserFieldType.java
@@ -1,12 +1,19 @@
 package com.cron.parser;
 
 public enum CronParserFieldType {
-    MINUTES(0, 59), HOURS(0, 23), DAY_OF_MONTH(1, 31), MONTH(1, 12), DAY_OF_WEEK(1, 7);
+    MINUTES("minute", 0, 59),
+    HOURS("hour", 0, 23),
+    DAY_OF_MONTH("day of month", 1, 31),
+    MONTH("month", 1, 12),
+    DAY_OF_WEEK("day of week", 1, 7);
 
-    public final int max;
+    public final String description;
     public final int min;
+    public final int max;
 
-    CronParserFieldType(int min, int max) {
+
+    CronParserFieldType(String description, int min, int max) {
+        this.description = description;
         this.min = min;
         this.max = max;
     }
